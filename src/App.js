c79facaeee8b40c10f00ed7d037542d39bc47abf
ezhil <ezhilharish1@gmail.com>
2024-02-17 08:33:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Tictacteo from './components/Tictacteo/Tictacteo'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Tictacteo/>
+    </div>
+  );
+}
+
+export default App;
